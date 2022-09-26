98c6a212be252669892a7e83f86d281b9f1c254c
Martin Olmedo <martinolmedo078@gmail.com>
2022-09-26 02:00:40 -0300

    Se termina componente welcome

--- frontend-web/src/assets/script.js
+++ frontend-web/src/assets/script.js
@@ -3,10 +3,13 @@ function changeClass(){
     const titulo = document.getElementById('titulo-sidebar')
     const lista = document.getElementById('lista-botones')
     const header = document.getElementById('contenedor-header')
+    const welcome = document.getElementById('contenedor-welcome')
     
     sidebar.classList.toggle('sidebar-check')
     titulo?.classList.toggle('titulo-check')
     lista?.classList.toggle('lista-botones-check')
     header?.classList.toggle('contenedor-header')
     header?.classList.toggle('contenedor-header-check')
+    welcome?.classList.toggle('contenedor-welcome')
+    welcome?.classList.toggle('contenedor-welcome-check')
   } 
