a9295056c381983817aabf58186dd3fdead290bf
Martin Olmedo <martinolmedo078@gmail.com>
2022-09-26 03:03:47 -0300

    Se termina componente prestamos

--- frontend-web/src/assets/script.js
+++ frontend-web/src/assets/script.js
@@ -4,6 +4,7 @@ function changeClass(){
     const lista = document.getElementById('lista-botones')
     const header = document.getElementById('contenedor-header')
     const welcome = document.getElementById('contenedor-welcome')
+    const lending = document.getElementById('contenedor-lendings')
     
     sidebar.classList.toggle('sidebar-check')
     titulo?.classList.toggle('titulo-check')
@@ -12,4 +13,6 @@ function changeClass(){
     header?.classList.toggle('contenedor-header-check')
     welcome?.classList.toggle('contenedor-welcome')
     welcome?.classList.toggle('contenedor-welcome-check')
+    lending?.classList.toggle('contenedor-lendings')
+    lending?.classList.toggle('contenedor-lendings-check')
   } 
